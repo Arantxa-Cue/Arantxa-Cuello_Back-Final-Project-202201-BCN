d39f92c7be4c17eb88603ec41d521829fcf372ae
Arantxa Cuello <arantxa_312@hotmail.com>
2022-03-09 16:17:47 +0100

    database connected

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+require("dotenv").config();
+const debug = require("debug")("mindfulness:root");
+const chalk = require("chalk");
+const mindServidor = require("./server/server");
+const app = require("./server/index");
+const connectDatabase = require("./database/index");
 
+const port = process.env.PORT || 5000;
+const connectionString = process.env.MONGO_STRING;
+
+(async () => {
+  try {
+    await connectDatabase(connectionString);
+    await mindServidor(port, app);
+  } catch (error) {
+    debug(chalk.redBright(`Error: ${error.message}`));
+  }
+})();
